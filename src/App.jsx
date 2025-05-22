7ee81133430c23cc93c5bca1d44c819ed3128974
C3sar10 <ciperez3@my.canyons.edu>
2025-05-21 21:13:30 -0700

    Template portfolio starter with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl text-green-500 font-bold underline">Tailwind Works!</h1>
+    </>
+  )
+}
+
+export default App
